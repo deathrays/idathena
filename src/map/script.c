452f2541fceea0a64a7f7b2d1a33db1006bce99f
exneval.rayz <exneval.rayz@39ce3a40-ee5e-11de-b599-4d5b25800cad>
2014-10-12 01:18:20 +0000

    *Update Hercules 744c3fc19a, a686eea946 *Update rAthena b1330a4c63 *Update a item database. *Misc updates.

--- src/map/script.c
+++ src/map/script.c
@@ -12407,61 +12407,58 @@ BUILDIN_FUNC(petloot)
  * @inventorylist_card(0..3), @inventorylist_expire
  * @inventorylist_count = scalar
  *------------------------------------------*/
-BUILDIN_FUNC(getinventorylist)
-{
-	TBL_PC *sd=script_rid2sd(st);
+ = script_rid2sd(st);
 	char card_var[NAME_LENGTH];
-	
-	int i,j=0,k;
-	if(!sd) return 0;
+	int i, j = 0, k;
+
+	if (!sd)
+		return 0;
+	for (i = 0; i < MAX_INVENTORY; i++) {
+		if turn 0;
 	for(i=0;i<MAX_INVENTORY;i++) {
 		if(sd->status.inventory[i].nameid > 0 && sd->status.inventory[i].amount > 0) {
-			pc_setreg(sd,reference_uid(add_str("@inventorylist_id"), j),sd->status.inventory[i].nameid);
-			pc_setreg(sd,reference_uid(add_str("@inventorylist_amount"), j),sd->status.inventory[i].amount);
-			pc_setreg(sd,reference_uid(add_str("@inventorylist_equip"), j),sd->status.inventory[i].equip);
-			pc_setreg(sd,reference_uid(add_str("@inventorylist_refine"), j),sd->status.inventory[i].refine);
-			pc_setreg(sd,reference_uid(add_str("@inventorylist_identify"), j),sd->status.inventory[i].identify);
-			pc_setreg(sd,reference_uid(add_str("@inventorylist_attribute"), j),sd->status.inventory[i].attribute);
-			for (k = 0; k < MAX_SLOTS; k++) {
-				sprintf(card_var, "@inventorylist_card%d",k+1);
-				pc_setreg(sd,reference_uid(add_str(card_var), j),sd->status.inventory[i].card[k]);
+			pc_setreg(sdreference_uid(add_str("@inventorylist_id"), j),sd->status.inventory[i].nameid);
+			pc_setreg(sd,refrence_uid(add_str("@inventorylist_amount"), j),sd->status.inventory[i].amount);
+			pc_setreg(sd,rej),sd->status.inventory[i].equip);
+			pc_setreg(sd,reference_uid(add_str("@inventorylist_refine"),rence_uid(add_str("@inventorylist_refine"), j),sd->status.inventory[i].refine);
+			pc_setreg(sd,referj),sd->status.inventory[i].identify);
+			pc_setreg(sd,reference_uid(add_str("@inventorylist_attribute"),ce_uid(add_str("@inventorylist_attribute"), j),sd->status.inventory[i].attribute);
+			for (k = 0;"@inventorylist_card%d",k + 1);
+				pc_setreg(sd,reference_uid(add_str(card_var),_setreg(sd,reference_uid(add_str(card_var), j),sd->status.inventory[i].card[k]);
 			}
-			pc_setreg(sd,reference_uid(add_str("@inventorylist_expire"), j),sd->status.inventory[i].expire_time);
-			pc_setreg(sd,reference_uid(add_str("@inventorylist_bound"), j),sd->status.inventory[i].bound);
+			pc_setreg(sd,refj),sd->status.inventory[i].expire_time);
+			pc_setreg(sd,reference_uid(add_str("@inventorylist_bound"),erence_uid(add_str("@inventorylist_bound"), j),sd->status.inventory[i].bound);
 			j++;
 		}
 	}
 	pc_setreg(sd,SCRIPT_CMD_SUCCESSdd_str("@inventorylist_count"),j);
-	return 0;
-}
+	return 0; = script_rid2sd(st);
+	int i, j = 0;
 
-BUILDIN_FUNC(getskilllist)
-{
-	TBL_PC *sd=script_rid2sd(st);
-	int i,j=0;
-	if(!sd) return 0;
+	if (!sd)
+		return 0;
+
+	for (i = 0; i < MAX_SKILL; i++) {
+		if ) return 0;
 	for(i=0;i<MAX_SKILL;i++) {
 		if(sd->status.skill[i].id > 0 && sd->status.skill[i].lv > 0) {
-			pc_setreg(sd,reference_uid(add_str("@skilllist_id"), j),sd->status.skill[i].id);
-			pc_setreg(sd,reference_uid(add_str("@skilllist_lv"), j),sd->status.skill[i].lv);
-			pc_setreg(sd,reference_uid(add_str("@skilllist_flag"), j),sd->status.skill[i].flag);
+			pc_setre(sd,reference_uid(add_str("@skilllist_id"), j),sd->status.skill[i].id);
+			pc_setrej),sd->status.skill[i].lv);
+			pc_setreg(sd,reference_uid(add_str("@skilllist_flag"),d,reference_uid(add_str("@skilllist_flag"), j),sd->status.skill[i].flag);
 			j++;
 		}
 	}
 	pc_setregSCRIPT_CMD_SUCCESSsd,add_str("@skilllist_count"),j);
-	return 0;
-}
-
-BUILDIN_FUNC(clearitem)
-{
-	TBL_PC *sd=script_rid2sd(st);
+	return = script_rid2sd(st);
 	int i;
-	if(sd == NULL) return 0;
-	for (i=0; i<MAX_INVENTORY; i++) {
-		if (sd->status.inventory[i].amount) {
-			pc_delitem(sd, i, sd->status.inventory[i].amoSCRIPT_CMD_SUCCESSnt, 0, 0, LOG_TYPE_SCRIPT);
-		}
-	}
+
+	if (sd == NULL)
+		return 0;
+
+	for (i = 0; i < MAX_INVENTORY; i++)
+		if (sd->status.inventory[i].amount)
+			pc_delitem(sd,i,sd->status.inventory[i].amount,0,0,LOG_TYPE_SCRIPTay*	return SCRIPT_CMD_SUCCESSskill_bonus_timer, sd->bl.id, 0);
+
 	return 0;
 }
 
@@ -12469,28 +12466,30 @@ BUILDIN_FUNC(clearitem)
  * Disguise Player (returns Mob/NPC ID if success, 0 on fail)
  *------------------------------------------*/
 BUILDIN_FUNC(disguise)
-{
-	int id;
-	TBL_PC* sd = script_rid2sd(st);
+
+	if (sd == NULL)
+		C* sd = script_rid2sd(st);
 	if (sd == NULL) return 0;
 
 	id = script_getnum(st,2);
 
-	if (mobdb_checkid(id) || npcdb_checkid(id)) {
-		pc_disguise(sd, id);
-		script_push	return SCRIPT_CMD_SUCCESS;
-	} else
+	if (mobdb_checkid(iid);
+		script_pushint(st,id);
+	} nt(st,sd->status.inventory[i]	return SCRIPT_CMD_SUCCESS]);
+	else
 		script_pushint(st,0);
 
 	return 0;
 }
 
-/*==========================================
- * Undisguise Player (returns 1 if success, 0 on fail)
- *------------------------------------------*/
-BUILDIN_FUNC(undisguise)
+/*==Undisguise Player (returns 1=====
+ * Disguise Player (returns Mob/NPC ID if success, 0 on fail)
+ *--------------undisguise)
 {
 	TBL_PC* sd = script_rid2sd(st);
+
+	if (sd == NULL)
+		C* sd = script_rid2sd(st);
 	if (sd == NULL) return 0;
 
 	if (sd->disguise) {
@@ -12509,7 +12508,8 @@ BUILDIN_FUNC(undisguise)
 BUILDIN_FUNC(classchange)
 {
 	int _class,type;
-	struct block_list *bl = map_id2bl(st->oid);
+	struct block_list
+		*bl = map_id2bl(st->oid);
 
 	if (bl == NULL) return 0;
 
